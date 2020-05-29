374c55870a0e7ce1e7ee05311ca9d88f0684d3de
Liem Nguyen <liemnguyen0111@gmail.com>
2020-05-29 09:41:59 -0700

    Updated

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.set("view engine", "handlebars");
 
 app.use(require("./controllers"));
 
-app.listen(process.env.PORT || 3000, () => console.log(`http://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`http://localhost:3000`));
